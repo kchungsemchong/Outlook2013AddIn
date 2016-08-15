b467746b59e31c8750c1eae4e801ed8acc8237e6
kchungsemchong <kchungsemchong@gmail.com>
2016-08-15 18:15:52 +0400

    Initial Commit

---
+++ Outlook2013AddIn/Language.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+
+namespace Outlook2013AddIn
+{
+    public class Language
+    {
+        public int FindCurrentLanguage()
+        {
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
+            int localeId = currentCulture.LCID;
+
+            return localeId;
+        }
+
+        public Dictionary<string, string> GetEnglishLabels()
+        {
+            //I tried to add the labels into the Resource but it didn't work
+            //using (ResXResourceWriter resx = new ResXResourceWriter(@".\Ribbon1.resx"))
+            //{
+            //    resx.AddResource("btnAttachArtifactLabel", "Attach item");
+            //    resx.AddResource("btnAttachArtifactScreenTip", "Attach artifacts from WiNPAT");
+            //}
+
+            var englishLabels = new Dictionary<string, string>()
+            {
+                {"btnAttachArtifactLabel", "Attach item"},
+                {"btnAttachArtifactScreenTip", "Attach artifacts from WiNPAT"}
+            };
+
+            return englishLabels;
+        }
+
+        public Dictionary<string, string> VerifyLocaleId(int localeId)
+        {
+            Dictionary<string, string> englishLabels = null;
+            var localeIds = new List<int>()
+            {
+                1033,
+                2057
+            };
+            bool isInList = localeIds.IndexOf(localeId) != -1;
+            if (isInList == true)
+            {
+                if (!(localeId <= 0))
+                {
+                    englishLabels = GetEnglishLabels();
+                }
+            }
+
+
+            return englishLabels;
+        }
+
+
+    }
+}
---
+++ Outlook2013AddIn/Winpat.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Outlook2013AddIn
+{
+    partial class Winpat : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Winpat()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Winpat));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.grpWinpat = this.Factory.CreateRibbonGroup();
+            this.btnAttachArtifact = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.grpWinpat.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabNewMailMessage";
+            this.tab1.Groups.Add(this.grpWinpat);
+            this.tab1.Label = "TabNewMailMessage";
+            this.tab1.Name = "tab1";
+            // 
+            // grpWinpat
+            // 
+            this.grpWinpat.Items.Add(this.btnAttachArtifact);
+            this.grpWinpat.Label = "WiNPAT";
+            this.grpWinpat.Name = "grpWinpat";
+            this.grpWinpat.Position = this.Factory.RibbonPosition.BeforeOfficeId("GroupIncludeMainTab");
+            // 
+            // btnAttachArtifact
+            // 
+            this.btnAttachArtifact.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnAttachArtifact.Image = ((System.Drawing.Image)(resources.GetObject("btnAttachArtifact.Image")));
+            this.btnAttachArtifact.Label = "Dokument anhängen";
+            this.btnAttachArtifact.Name = "btnAttachArtifact";
+            this.btnAttachArtifact.ScreenTip = "Bringen Sie Artefakte aus WiNPAT";
+            this.btnAttachArtifact.ShowImage = true;
+            this.btnAttachArtifact.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnAttachArtifact_Click);
+            // 
+            // Winpat
+            // 
+            this.Name = "Winpat";
+            this.RibbonType = "Microsoft.Outlook.Mail.Compose, Microsoft.Outlook.Mail.Read";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Winpat_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.grpWinpat.ResumeLayout(false);
+            this.grpWinpat.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpWinpat;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnAttachArtifact;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Winpat Winpat
+        {
+            get { return this.GetRibbon<Winpat>(); }
+        }
+    }
+}
